16b71238d6d258cd9dacceca0c041e7ae6959915
brillix <baruch@brillix.co.il>
2015-11-03 09:32:48 +0200

    add .Config to scripts

---
+++ oracle/PostInstallOracle.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+#
+# Install oracle
+#
+# description:
+#         this script need to run by admin user
+#
+#   3.11.2015 - created by baruch o
+#   2.11.2015 - add
+#
+#
+#
+##############GLOBAL PARAM#################
+. Config.conf
+#############################################
+#chack if oracle install finish
+
+#run root.sh
+if [[ $DEBUG -ne 0 ]] ; then echo "run root.sh as root"; fi
+sudo -H sh -c $ORACLE_HOME/root.sh
